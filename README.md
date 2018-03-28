17ed223a2cd2e0d8e649108ca1865f0d3ed1236f
Jurgen Treep <jurgen.treep@gmail.com>
2018-03-28 22:30:16 +0200

    Creat a product detail view and a means to switch between them.
    Also did some css to position the images on top of each other so that I can animate them later.

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # product-display
 
+I've chosen not to use ES6 features because that would require me to set up a build system which would compile the javascript back to code which is supported by most browsers. I could've chosen to only support the latest browsers but I like the challenge I find in these limitations.
+
+I've really come to appriciate template engines because they make rendering and updating the html so much easier than trying to do update every html element yourself.
+
+First I thought there wouldn't be much logic and I could keep it all in one javascript file and it would still be readable but I'm now at the point where it's already becoming pretty complicated. I might have to look at splitting the code up in different files and creating objects which provide functionality. Classes would be nice but that's also an ES6 feature.
+
 ## Time
 
 ### 2018-03-26 20:00:00 - 2018-03-26 20:50:00
 * Build html
 * Retrieve resource from api
 * Render simple list
+
+### 2018-03-28 20:00:00 - 2018-03-28 22:30:00
+* Render html for product detail
+* Add a little css to put the images on top of each other so we can animate them later
+* Switch between the two different views
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var productList = [];
 var mainElement = document.querySelector('main');
+var productListElement = null;
+var backButtonElement = document.querySelector('.back-button');
+var productElement = null;
 
 function renderProductList(productList) {
-    var list = document.createElement('ul');
+    var list = productListElement = document.createElement('ul');
+    list.className = 'product-list';
 
     productList.forEach(function (product) {
         list.appendChild(createListItem(product));
@@ -13,6 +17,8 @@ function renderProductList(productList) {
 
 function createListItem(product) {
     var listItem = document.createElement('li');
+    listItem.dataset.id = product.id;
+    listItem.addEventListener('click', showProduct);
 
     var thumbnail = document.createElement('img');
     thumbnail.src = product.images[0].thumb;
@@ -34,7 +40,8 @@ function getProducts(callback) {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            callback(JSON.parse(this.responseText));
+            productList = JSON.parse(this.responseText);
+            callback(productList);
         } else {
             displayError();
         }
@@ -43,11 +50,109 @@ function getProducts(callback) {
     request.send();
 }
 
+function getProductById(productId) {
+    for (var index = 0; index < productList.length; index++) {
+        if (productList[index].id === parseInt(productId)) {
+            return productList[index];
+        }
+    }
+}
+
+function showProduct(event) {
+    hideProductList();
+    showBackButton();
+    renderProduct(getProductById(event.currentTarget.dataset.id));
+}
+
+function hideProduct() {
+    mainElement.removeChild(productElement);
+}
+
+function showProductList() {
+    productListElement.style.display = 'initial';
+}
+
+function hideProductList() {
+    productListElement.style.display = 'none';
+}
+
+function showBackButton() {
+    backButtonElement.style.display = 'initial';
+}
+
+function hideBackButton() {
+    backButtonElement.style.display = 'none';
+}
+
+function renderProduct(product) {
+    var container = document.createElement('div');
+    container.className = 'product-detail';
+
+    var gallery = createGallery(product.images);
+    container.appendChild(gallery);
+
+    var thumbnails = createThumbnails(product.images);
+    container.appendChild(thumbnails);
+    
+    var title = document.createElement('h1');
+    title.textContent = product.title;
+    container.appendChild(title);
+
+    var descriptionParagraph = document.createElement('p');
+    descriptionParagraph.textContent = product.description;
+    container.appendChild(descriptionParagraph);
+
+    var specificationParagraph = document.createElement('p');
+    specificationParagraph.textContent = product.specification;
+    container.appendChild(specificationParagraph);
+
+    productElement = container;
+    mainElement.appendChild(container);
+}
+
+function createGallery(images) {
+    var container = document.createElement('div');
+    container.className = 'gallery';
+    
+    var imageHolder = document.createElement('div');
+    imageHolder.className = 'image-holder';
+
+    images.forEach(function(image) {
+        var imageElement = document.createElement('img');
+        imageElement.src = image.original;
+        imageHolder.appendChild(imageElement);
+    });
+
+    container.appendChild(imageHolder);
+
+    return container;
+}
+
+function createThumbnails(images) {
+    var container = document.createElement('div');
+    container.className = 'thumbnail-container';
+
+    images.forEach(function(image) {
+        var imageElement = document.createElement('img');
+        imageElement.src = image.thumb;
+        container.appendChild(imageElement);
+    });
+
+    return container;
+}
+
+function goBack() {
+    hideBackButton();
+    hideProduct();
+    showProductList();
+}
+
 function displayError() {
     // TODO: Display error
 }
 
 function initialize() {
+    backButtonElement.addEventListener('click', goBack);
     getProducts(renderProductList);
 }
 
